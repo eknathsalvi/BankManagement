1eb1f2bfdf6d411f1999269552b036693e6bc570
eknathsalvi <1nath.salvi@gmail.com>
2018-10-28 20:05:45 +0530

    deploy commit

--- customerInfo/urls.py
+++ customerInfo/urls.py
@@ -4,9 +4,11 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('customers', CustomerViewSet, base_name='customers')
+router.register('accounts', AccountViewSet, base_name='accounts')
 #router.register('transaction', transaction, base_name='transaction')
 urlpatterns = [
     path('transaction/', Transaction.as_view()),
+    path('', home),
 ]
 
 
